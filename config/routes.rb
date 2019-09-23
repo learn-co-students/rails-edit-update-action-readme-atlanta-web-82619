eb3b2cd86d05aacb167e71439326719577067942
Raza <rtshareef@gmail.com>
2019-09-23 00:52:37 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available
+  resources :articles, only: [:index, :show, :new, :create, :edit, :update]
+  
+  # within this file, see http://guides.rubyonrails.org/routing.html
 end
